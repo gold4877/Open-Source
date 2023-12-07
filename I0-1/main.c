62aa61dc2ab245bbe9242844ec24753dd81931bd
Oh Geumbin <gold4877@naver.com>
2023-12-07 18:09:21 +0900

    테트리스 음악 관련 주석 삭제

--- I0-1/main.c
+++ I0-1/main.c
@@ -6,14 +6,6 @@
 void main() {
 	CursorView(false);
 	system("mode con cols=76 lines=28 | title 오픈 소스 깃 업로드 테스트 ");
-	/*
-	아래의 PlaySound 메서드는 .wav 음악 파일을 재생시켜주는 함수입니다.
-	Controller.h 헤더파일에 include한 mmsystem.h 헤더파일을 통해 .wav음악을 출력시키는데
-	음막을 출력할 경우 해당 경로를 Controller.h에 BGM의 경로를 문자열로 지정하면 됩니다
-	그후 아래 PlaySound 주석 해제
-	음원은 저작권 문제로 제공 x */
-
-	// PlaySound(Text(BGM), NULL, SND_FILENAME | SAN_ASYNC | SND_LOOP);
 
 	while (true) {
 		GameTitle();
